b76f3f682f3c72cfc970ea87e54cc31da3bd74d9
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-11-05 00:13:15 -0300

    creacion de formulario del usuario y los herlper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :histories
-
-  root "histories#index"
+  resources :sessions, only: [:create, :destroy]
+  get 'users/sign_in', to: 'sessions#new'
+  root to: "histories#index"
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
